8784abdcfc0a65373e24db382894b37f30605e95
Jonathan <jonathanpalma89@gmail.com>
2020-04-30 00:43:43 +0100

    refactor: Home and App jsx

---
+++ frontend/src/components/home/Home.jsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import Main from '../templates/Main';
+
+export default props =>
+  <Main icon='home' title='Home'
+    subtitle='2nd Project in React...'>
+    <div className='display-4'>Welcome!</div>
+    <hr />
+    <p className='mb-0'>This project is part of the International History Students & Historians Group, which gives the possibility for any person to report a monument vicinity that is damaged, in bad condition or scheduled for destruction.</p>
+  </Main>
--- frontend/src/main/App.jsx
+++ frontend/src/main/App.jsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import Logo from '../components/templates/Logo';
 import Nav from '../components/templates/Nav';
-import Main from '../components/templates/Main';
+import Home from '../components/home/Home';
 import Footer from '../components/templates/Footer';
 
 import 'bootstrap/dist/css/bootstrap.min.css'
@@ -12,11 +12,6 @@ export default props =>
   <div className='app'>
     <Logo />
     <Nav />
-    <Main icon='home' title='Home'
-      subtitle='2nd Project in React...'>
-      <div className='display-4'>Welcome!</div>
-      <hr />
-      <p className='mb-0'>This project is part of the International History Students & Historians Group, which gives the possibility for any person to report a monument vicinity that is damaged, in bad condition or scheduled for destruction.</p>
-    </Main>
+    <Home />
     <Footer />
   </div>
