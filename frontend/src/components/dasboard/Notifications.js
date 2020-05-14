fcebbcfbc09a58dfdad7d4b83594c155c399670e
Jonathan <jonathanpalma89@gmail.com>
2020-05-14 20:12:22 +0100

    style: notifications' component

--- frontend/src/components/dasboard/Notifications.js
+++ frontend/src/components/dasboard/Notifications.js
@@ -1,9 +1,19 @@
 import React from 'react';
 
-const Notifications = () => {
+const Notifications = (props) => {
   return (
     <section>
-      <p>Notifications</p>
+      <div className='card z-deph-0'>
+        <div className='card-content'>
+          <span className='card-title'>Notifications</span>
+          <ul className='notification'>
+            <li>Notification</li>
+            <li>Notification</li>
+            <li>Notification</li>
+            <li>Notification</li>
+          </ul>
+        </div>
+      </div>
     </section>
   )
 }
