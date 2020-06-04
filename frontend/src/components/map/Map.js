7fe2afc3d739894ba7afc83e4287f8c8fc70fb49
Jonathan <jonathanpalma89@gmail.com>
2020-06-04 17:50:11 +0100

    style: add geocoder input location

--- frontend/src/components/map/Map.js
+++ frontend/src/components/map/Map.js
@@ -1,5 +1,5 @@
 import React, { Component, Fragment } from 'react';
-import MapboxGeocoder from 'mapbox-gl-geocoder';
+import MapboxGeocoder from '@mapbox/mapbox-gl-geocoder';
 import Main from '../templates/Main';
 // import ReportForm from './ReportForm';
 import mapboxgl from 'mapbox-gl';
@@ -47,6 +47,9 @@ class Map extends Component {
 
     const geocoder = new MapboxGeocoder({
       accessToken: mapboxgl.accessToken,
+      marker: {
+        color: 'orange'
+      },
       mapboxgl: mapboxgl
     });
     document.getElementById('geocoder').appendChild(geocoder.onAdd(map));
@@ -73,100 +76,6 @@ class Map extends Component {
       });
     });
 
-    const pulsingDot = {
-      width: 100,
-      height: 100,
-      data: new Uint8Array(100 * 100 * 4),
-
-      // get rendering context for the map canvas when layer is added to the map
-      onAdd: function () {
-        const canvas = document.createElement('canvas');
-        canvas.width = this.width;
-        canvas.height = this.height;
-        this.context = canvas.getContext('2d');
-      },
-
-      // called once before every frame where the icon will be used
-      render: function () {
-        const duration = 1000;
-        const t = (performance.now() % duration) / duration;
-
-        const radius = (100 / 2) * 0.3;
-        const outerRadius = (100 / 2) * 0.7 * t + radius;
-        const context = this.context;
-
-        // draw outer circle
-        context.clearRect(0, 0, this.width, this.height);
-        context.beginPath();
-        context.arc(
-          this.width / 2,
-          this.height / 2,
-          outerRadius,
-          0,
-          Math.PI * 2
-        );
-        context.fillStyle = 'rgba(255, 200, 200,' + (1 - t) + ')';
-        context.fill();
-
-        // draw inner circle
-        context.beginPath();
-        context.arc(
-          this.width / 2,
-          this.height / 2,
-          radius,
-          0,
-          Math.PI * 2
-        );
-        context.fillStyle = 'rgba(255, 100, 100, 1)';
-        context.strokeStyle = 'white';
-        context.lineWidth = 2 + 4 * (1 - t);
-        context.fill();
-        context.stroke();
-
-        // update this image's data with data from the canvas
-        this.data = context.getImageData(
-          0,
-          0,
-          this.width,
-          this.height
-        ).data;
-
-        // continuously repaint the map, resulting in the smooth animation of the dot
-        map.triggerRepaint();
-
-        // return `true` to let the map know that the image was updated
-        return true;
-      }
-    };
-
-    map.on('load', function () {
-      map.addImage('pulsing-dot', pulsingDot, { pixelRatio: 2 });
-
-      map.addSource('points', {
-        'type': 'geojson',
-        'data': {
-          'type': 'FeatureCollection',
-          'features': [
-            {
-              'type': 'Feature',
-              'geometry': {
-                'type': 'Point',
-                'coordinates': [-0.2157315, 50.915691]
-              }
-            }
-          ]
-        }
-      });
-      map.addLayer({
-        'id': 'points',
-        'type': 'symbol',
-        'source': 'points',
-        'layout': {
-          'icon-image': 'pulsing-dot'
-        }
-      });
-    });
-
     // clean up on unmount
     return () => map.remove();
   }
@@ -181,23 +90,25 @@ class Map extends Component {
               <span className='content-name'>Title</span>
             </label>
           </div>
-          <div className="form-group col-12 d-inline-block pt-3 pl-0">
+          <div className='row'>
+            <div className="geocoder col-12 col-lg-6 d-inline-block pl-4 p-3" id="geocoder" required />
+            <div className="form-group col-12 col-lg-6 d-inline-block p-3">
+              <label className='pt-1 pr-1' htmlFor="type">Type:</label>
+              <select defaultValue='Monument' className='p-1' name="type" id="type" onChange={this.updateFields} required>
+                <option value="Monument">Monument</option>
+                <option value="Site">Site</option>
+                <option value="Building">Building</option>
+                <option value="Object">Object</option>
+                <option value="Archeological site">Archeological site</option>
+              </select>
+            </div>
+          </div>
+          <div className="form-group col-12 d-inline-block pl-0">
             <div className="form-textarea">
               <textarea placeholder='Describe what you have discovered...' rows='8' className='textarea-input' style={{ resize: 'none' }}
                 id='description' onChange={this.updateFields} required />
             </div>
           </div>
-          <div className="form-group col-6 d-inline-block pt-3 pl-0">
-            <label className='pt-1 pr-1' htmlFor="type">Type:</label>
-            <select defaultValue='Monument' className='p-1' name="type" id="type" onChange={this.updateFields} required>
-              <option value="Monument">Monument</option>
-              <option value="Site">Site</option>
-              <option value="Building">Building</option>
-              <option value="Object">Object</option>
-              <option value="Archeological site">Archeological site</option>
-            </select>
-          </div>
-          <div className="geocoder" id="geocoder" required />
         </div>
       </Fragment>
     )
