970c93b60007d1124817dde43a4ca19267a7822f
Jonathan <jonathanpalma89@gmail.com>
2020-06-18 19:14:30 +0100

    chore: start to organise UserDetais component

--- frontend/src/components/users/UserDetails.js
+++ frontend/src/components/users/UserDetails.js
@@ -12,9 +12,25 @@ const headerProps = {
 const UserDetails = (props) => {
   return (
     <Main {...headerProps}>
-      <p>{props.profile.firstName}</p>
-      <p>{props.profile.lastName}</p>
-      <p>{props.auth.email}</p>
+      <div className='container'>
+        <center>
+          <h2 className='user-title'>{props.profile.firstName} {props.profile.lastName}</h2>
+          <h3>Welcome to YourSpace.</h3>
+        </center>
+        <div className="row pt-5 pb-5">
+          <div className="col-12 col-lg-4">
+            <p>Profile Picture</p>
+          </div>
+          <div className="col-12 col-lg-8">
+            <label>First Name</label>
+            <h5>{props.profile.firstName}</h5>
+            <label>Last Name</label>
+            <h5>{props.profile.lastName}</h5>
+            <label>Email</label>
+            <h5>{props.auth.email}</h5>
+          </div>
+        </div>
+      </div>
     </Main>
   )
 }
