1bce6daf0937e3c91723d11982fa6d4272653c0d
Jonathan <jonathanpalma89@gmail.com>
2020-07-27 21:06:05 +0100

    fix: upload user details

--- frontend/src/components/users/UpdateUser.js
+++ frontend/src/components/users/UpdateUser.js
@@ -7,7 +7,7 @@ import { connect } from 'react-redux';
 import { updateUser } from '../../store/actions/updateAuthActions';
 
 class UpdateUser extends Component {
-  constructor(props){
+  constructor(props) {
     super(props);
     this.state = {
       profile: {
@@ -19,13 +19,20 @@ class UpdateUser extends Component {
       picture: null
     }
     this.updateFields = this.updateFields.bind(this);
+    this.handleUpload = this.handleUpload.bind(this);
     this.handleClick = this.handleClick.bind(this);
   }
 
   updateFields = (event) => {
     this.setState({
-      [event.target.id]: event.target.value
+      profile: {
+        ...this.state.profile,
+        [event.target.id]: event.target.value
+      }
     });
+  }
+  
+  handleUpload = (event) => {
     if (event.target.files[0]) {
       const picture = event.target.files[0];
       this.setState({
@@ -37,88 +44,97 @@ class UpdateUser extends Component {
   handleClick = (event) => {
     event.preventDefault();
     const { picture } = this.state;
-    const uploadPic = storage.ref(`images/${picture.name}`).put(picture);
-    uploadPic.on('state_changed',
-      (snapshot) => {
+    if (this.state.profile.firstName && this.state.profile.lastName && this.state.profile.email !== '') {
+      if (picture) {
+        const uploadPic = storage.ref(`images/${picture.name}`).put(picture);
+        uploadPic.on('state_changed',
+          (snapshot) => {
 
-      },
-      (error) => {
-        console.log(error);
-      },
-      () => {
-        storage.ref('images').child(picture.name).getDownloadURL()
-          .then(url => {
-            this.setState({
-              profile: {
-                ...this.state.profile,
-                pictureURL: url
-              }
-            });
-          }).then(() => {
-            if (this.state.profile.firstName && this.state.profile.lastName && this.state.profile.email !== '') {
-              this.props.updateUser(this.state.profile);
-              alert("Post updated with success.")
-            } else {
-              alert("All fields most be fielded.");
-            }
+          },
+          (error) => {
+            console.log(error);
+          },
+          () => {
+            storage.ref('images').child(picture.name).getDownloadURL()
+              .then(url => {
+                this.setState({
+                  profile: {
+                    ...this.state.profile,
+                    pictureURL: url
+                  }
+                });
+              }).then(() => {
+                this.props.updateUser(this.state.profile);
+                alert("Post updated with success.")
+              })
+
           })
-      })
+      }
+      else {
+        this.props.updateUser(this.state.profile);
+        alert("Post updated with success.")
+      }
+    }
+    else {
+      alert("All fields most be fielded.");
+    }
   }
 
-  render() {
-    const { pictureURL } = this.state.profile;
-    return (
-      <div className="form container pb-2">
-        <div className="row">
-          <div className="col-12">
-            <div className="form-group pt-2">
-              <input className='form-control' type='text' value={this.state.profile.firstName}
-                id='firstName' onChange={this.updateFields} required />
-              <label className='form-label' htmlFor='firstName'>
-                <span className='content-name'>First Name</span>
-              </label>
+
+    render() {
+      const { pictureURL } = this.state.profile;
+      return (
+        <div className="form container pb-2">
+          <div className="row">
+            <div className="col-12">
+              <div className="form-group pt-2">
+                <input className='form-control' type='text' value={this.state.profile.firstName}
+                  id='firstName' onChange={this.updateFields} required />
+                <label className='form-label' htmlFor='firstName'>
+                  <span className='content-name'>First Name</span>
+                </label>
+              </div>
+            </div>
+            <div className="col-12">
+              <div className="form-group pt-2">
+                <input className='form-control' type='text' value={this.state.profile.lastName}
+                  id='lastName' onChange={this.updateFields} required />
+                <label className='form-label' htmlFor='lastName'>
+                  <span className='content-name'>Last Name</span>
+                </label>
+              </div>
+            </div>
+            <div className="col-12">
+              <div className="form-group pt-2">
+                <input className='form-control' type='text' value={this.state.profile.email}
+                  id='email' onChange={this.updateFields} required />
+                <label className='form-label' htmlFor='email'>
+                  <span className='content-name'>Last Name</span>
+                </label>
+              </div>
             </div>
           </div>
-          <div className="col-12">
-            <div className="form-group pt-2">
-              <input className='form-control' type='text' value={this.state.profile.lastName}
-                id='lastName' onChange={this.updateFields} required />
-              <label className='form-label' htmlFor='lastName'>
-                <span className='content-name'>Last Name</span>
-              </label>
-            </div>
+          <div className='d-flex justify-content-center'>
+            <input
+              type='file'
+              onChange={this.handleUpload}
+            />
+            <PicturePreview pictureURL={pictureURL} />
           </div>
-          <div className="col-12">
-            <div className="form-group pt-2">
-              <input className='form-control' type='text' value={this.state.profile.email}
-                id='email' onChange={this.updateFields} required />
-              <label className='form-label' htmlFor='email'>
-                <span className='content-name'>Last Name</span>
-              </label>
-            </div>
-          </div>
-        </div>
-        <div className='d-flex justify-content-center'>
-          <input
-            type='file'
-            onChange={this.updateFields}
-          />
-          <PicturePreview pictureURL={pictureURL} />
-        </div>
-        <div className="d-flex justify-content-end m-auto pt-5">
-          <button className="btn-input" onClick={this.handleClick}>
-            Update
+          <div className="d-flex justify-content-end m-auto pt-5">
+            <button className="btn-input" onClick={this.handleClick}>
+              Update
             </button>
+          </div>
         </div>
-      </div>
-    )
+      )
+    }
   }
-}
 
-const mapDispatchToProps = (dispatch) => {
-  return {
-    updateUser: (profile) => dispatch(updateUser(profile))
+  const mapDispatchToProps = (dispatch) => {
+    return {
+      updateUser: (profile) => dispatch(updateUser(profile))
+    }
   }
-}
 
-export default connect(null, mapDispatchToProps)(UpdateUser);
+  export default connect(null, mapDispatchToProps)(UpdateUser);
