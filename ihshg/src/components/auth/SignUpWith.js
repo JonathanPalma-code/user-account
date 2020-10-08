1ef6593f59f2b5c9c8b4adecf3b977c06d6e8f24
Jonathan <jonathanpalma89@gmail.com>
2020-10-08 21:45:44 +0100

    fix: functional signIn with FB or Google

--- ihshg/src/components/auth/SignUpWith.js
+++ ihshg/src/components/auth/SignUpWith.js
@@ -1,21 +1,29 @@
-import React from 'react';
-import firebase from 'firebase';
-import StyledFirebaseAuth from 'react-firebaseui/StyledFirebaseAuth';
+import React, { useEffect} from 'react';
+import firebase from 'firebase/app';
+// import StyledFirebaseAuth from 'react-firebaseui/StyledFirebaseAuth';
+import * as firebaseui from 'firebaseui'
 
 const SignInWith = () => {
-  const uiConfig = {
-    signInflow: 'popup',
-    signInOptions: [
-      firebase.auth.GoogleAuthProvider.PROVIDER_ID,
-      firebase.auth.FacebookAuthProvider.PROVIDER_ID
-    ],
-    callbacks: {
-      signInSuccessWithAuthResult: () => false
+
+  useEffect(() => {
+    const ui = firebaseui.auth.AuthUI.getInstance() || new firebaseui.auth.AuthUI(firebase.auth());
+    const uiConfig = {
+      callbacks: {
+        signInSuccessWithAuthResult: () => false
+      },
+      signInflow: 'popup',
+      signInOptions: [
+        firebase.auth.GoogleAuthProvider.PROVIDER_ID,
+        firebase.auth.FacebookAuthProvider.PROVIDER_ID
+      ],
     }
-  }
+    ui.start('#firebaseui-auth-container', uiConfig);
+  });
 
   return (
-    <StyledFirebaseAuth uiConfig={uiConfig} firebaseAuth={firebase.auth()} />
+    <section>
+      <div id="firebaseui-auth-container"></div>
+    </section>
   )
 }
 
--- ihshg/src/components/auth/VerifyEmail.js
+++ ihshg/src/components/auth/VerifyEmail.js
@@ -13,16 +13,16 @@ const headerProps = {
 }
 
 const VerifyEmail = (props) => {
-  const { auth } = props;
+  // const { auth } = props;
 
-  const handleClick = () => {
-    if (!auth.emailVerified) {
-      props.verifyEmail();
-      alert("The link was sent with success.");
-    } else {
-      alert("The link was already sent. Please check your email inbox.");
-    }
-  }
+  // const handleClick = () => {
+  //   if (!auth.emailVerified) {
+  //     props.verifyEmail();
+  //     alert("The link was sent with success.");
+  //   } else {
+  //     alert("The link was already sent. Please check your email inbox.");
+  //   }
+  // }
 
   return (
     <Main {...headerProps}>
@@ -31,9 +31,9 @@ const VerifyEmail = (props) => {
           <i className="fa fa-exclamation-triangle" aria-hidden="true" />
           <div>Your email is not verified.</div>
           <div className='message'>To have full access on your account, you need to go to your email inbox and verify your email.</div>
-          <div className='button-container'>
+          {/* <div className='button-container'>
             <button className='btn-subs mt-5 col-12 col-md-6' onClick={() => handleClick()}>Re-send verification email<span className='circle'></span></button>
-          </div>
+          </div> */}
         </div>
       </div>
     </Main>
